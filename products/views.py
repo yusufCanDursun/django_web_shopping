6c220df010b47355eb50eef6cb27aa52a687ed17
Yusuf Can Dursun <ycdursun2992@gmail.com>
2025-04-30 20:19:39 +0300

    added views, urls and models for products app

---
+++ products/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Product
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'products/product_list.html', {'products': products})
+
+def product_detail(request, id):
+    product = get_object_or_404(Product, id=id)
+    return render(request, 'products/product_detail.html', {'product': product})
