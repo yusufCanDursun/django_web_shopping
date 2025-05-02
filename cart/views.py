d0580196188850b76b79210f167904bf53c9b70b
Yusuf Can Dursun <ycdursun2992@gmail.com>
2025-05-02 22:03:51 +0300

    CRUD operations added to views

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render, redirect
-from .models import Cart
+from .models import Cart,CartItem
 from products.models import Product
+from rest_framework import viewsets
+from .serializers import CartItemSerializer
+
+
+
+class CartItemViewSet(viewsets.ModelViewSet):
+    queryset = CartItem.objects.all()
+    serializers_class = CartItemSerializer
 
 def cart_view(request):
     cart = Cart.objects.get(user=request.user)
@@ -9,5 +17,5 @@ def cart_view(request):
 def add_to_cart(request, product_id):
     product = get_object_or_404(Product, id=product_id)
     cart, created = Cart.objects.get_or_create(user=request.user)
-    cart.products.add(product)  # Örneğin, ürün sepete ekleniyor
+    cart.products.add(product) 
     return redirect('cart_view')
--- products/views.py
+++ products/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
+from rest_framework import viewsets
+from .serializers import ProductSerializers
+
+
+class ProductViewSet(viewsets.ModelViewSet): 
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializers
 
 def product_list(request):
     products = Product.objects.all()
     return render(request, 'products/product_list.html', {'products': products})
 
 def product_detail(request, id):
-    product = get_object_or_404(Product, id=id)
+    product = get_object_or_404(Product,id=id)
     return render(request, 'products/product_detail.html', {'product': product})
+
