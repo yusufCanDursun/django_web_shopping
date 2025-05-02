f3330a18bcb99ca9f82184d36c606dc17e8aa23f
Yusuf Can Dursun <ycdursun2992@gmail.com>
2025-05-02 22:09:26 +0300

    Added API endpoints to urls

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
+from .views import ProductViewSet
+
+
+router = DefaultRouter()
+router.register(r'products',ProductViewSet, basename='product')
 
 urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('<int:id>/', views.product_detail, name='product_detail'),
+    path('api/', include (router.urls)),
 ]
