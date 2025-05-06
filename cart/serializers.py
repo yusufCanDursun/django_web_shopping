8bfda0128290568bb4e30088fd25a5c0b762ebd4
Yusuf Can Dursun <ycdursun2992@gmail.com>
2025-05-06 23:06:33 +0300

    serializers updated

--- cart/serializers.py
+++ cart/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
 from .models import CartItem
+from products.models import Product  
 
 class CartItemSerializer(serializers.ModelSerializer):
+    product_id = serializers.PrimaryKeyRelatedField(
+        queryset=Product.objects.all(),
+        source='product',
+        write_only=True
+    )
+    
     class Meta:
         model = CartItem
-        fields = ['id', 'cart', 'product', 'quantity']
+        fields = ['id', 'cart', 'product', 'quantity', 'product_id']
+        read_only_fields = ['cart', 'product']  
