ffdd313808fe84429e499d00e50bbaa6ba7815df
Yusuf Can Dursun <ycdursun2992@gmail.com>
2025-05-06 23:08:20 +0300

    updated views for cart

--- cart/views.py
+++ cart/views.py
@@ -3,13 +3,18 @@ from .models import Cart,CartItem
 from products.models import Product
 from rest_framework import viewsets
 from .serializers import CartItemSerializer
-
+from rest_framework.permissions import IsAuthenticated
 
 
 class CartItemViewSet(viewsets.ModelViewSet):
     queryset = CartItem.objects.all()
-    serializers_class = CartItemSerializer
+    serializer_class = CartItemSerializer
+    permission_classes = [IsAuthenticated]
 
+    def perform_create(self, serializer):
+        cart, _ = Cart.objects.get_or_create(user=self.request.user)
+        serializer.save(cart=cart)
+        
 def cart_view(request):
     cart = Cart.objects.get(user=request.user)
     return render(request, 'cart/cart_view.html', {'cart': cart})
