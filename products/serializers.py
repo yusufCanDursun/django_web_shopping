5a125af436b1ca828972f543f3be7d333bd5a8cc
Yusuf Can Dursun <ycdursun2992@gmail.com>
2025-05-12 19:55:38 +0300

    Updated

--- products/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Product 
-
-class ProductSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = Product 
-        fields = ['id', 'name', 'description', 'price', 'stock']
