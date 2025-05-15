d2bcb361e84a2d8b7b079f23b2ef8a8b735a555d
Yusuf Can Dursun <ycdursun2992@gmail.com>
2025-05-15 19:24:37 +0300

    Updated settings for frontend connection

--- backend/eshop/eshop/settings.py
+++ backend/eshop/eshop/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'account',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -132,4 +134,39 @@ AUTH_USER_MODEL = 'account.User'  # app_name.ModelName
 AUTHENTICATION_BACKENDS = [
     'account.backends.EmailAuthBackend',
     'django.contrib.auth.backends.ModelBackend',
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_METHODS = [
+    'POST',
+    'OPTIONS',  # OPTIONS isteği için
+]
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+# İzin verilen HTTP başlıkları
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
 ]
