92763a86350176a61c40a566bed14d90f74ea565
Yusuf Can Dursun <ycdursun2992@gmail.com>
2025-05-15 19:17:51 +0300

    update routes

--- frontend/eshop_front/src/App.jsx
+++ frontend/eshop_front/src/App.jsx
@@ -4,6 +4,8 @@ import Footer from './components/Footer';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import SellerPanel from './pages/SellerPanel';
+import Products from './pages/Products';
 import './App.css';
 
 function App() {
@@ -16,6 +18,9 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route path='/sellerpanel' element={<SellerPanel/>}/>
+            <Route path='/products' element={<Products/>}/>
+
           </Routes>
         </main>
         <Footer />
