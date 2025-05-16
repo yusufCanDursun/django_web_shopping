4f12f9cdd640f2e99868c1c215fad941b978962f
Yusuf Can Dursun <ycdursun2992@gmail.com>
2025-05-16 21:20:19 +0300

    added path

--- backend/eshop/account/urls.py
+++ backend/eshop/account/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
+    path('user/', views.user_info, name='user-info'),
 ]
--- backend/eshop/eshop/urls.py
+++ backend/eshop/eshop/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/admin/')),
     path('api/auth/', include('account.urls')),
 ]
