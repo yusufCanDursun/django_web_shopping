df4f5a22545f0dd6fb0fa7652deb7edb3ae4d24e
Yusuf Can Dursun <ycdursun2992@gmail.com>
2025-05-19 13:54:13 +0300

    update paths

--- backend/eshop/eshop/urls.py
+++ backend/eshop/eshop/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from django.views.generic import RedirectView
 
 
@@ -24,4 +26,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', RedirectView.as_view(url='/admin/')),
     path('api/auth/', include('account.urls')),
-]
+    path('api/products/', include('products.urls')),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
