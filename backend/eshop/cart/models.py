4a03235bc6c4f3d7e71541a310a2acff5828b8c1
Yusuf Can Dursun <ycdursun2992@gmail.com>
2025-05-22 23:36:57 +0300

    added cart class

---
+++ backend/eshop/cart/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.conf import settings
+
+from products.models import Product
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, related_name="items", on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def total_price(self):
+        return self.product.price * self.quantity
